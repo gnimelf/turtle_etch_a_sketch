d43c5770dfb58e20df834bb21911812c78921f44
gnimelf <gnimelf@users.noreply.github.com>
2021-02-02 17:32:09 -0500

    add file via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_foward():
+    tim.forward(10)
+
+
+def move_back():
+    tim.back(10)
+
+
+def turn_left():
+    tim.left(10)
+
+
+def turn_right():
+    tim.right(10)
+
+
+def clear():
+    tim.reset()
+    screen.reset()
+
+
+screen.listen()
+
+screen.onkey(move_foward, "Right")
+screen.onkey(move_back, "Left")
+screen.onkey(turn_left, "Up")
+screen.onkey(turn_right,"Down")
+screen.onkey(clear, "c")
+
+screen.exitonclick()
